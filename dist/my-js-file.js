dd8d91ab90e1fb995b66acfa750b31e18563a9e4
Nathan <15868262+NathanDreamweaver@users.noreply.github.com>
2023-09-22 15:21:00 +0200

    test

---
+++ dist/my-js-file.js
@@ -0,0 +1,40 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let dark = getCookie("dark");
+    if (dark != "") {
+        document.body.classList.toggle("dark")
+    }
+}
+
+function toggleDark() {
+    document.body.classList.toggle("dark")
+    let dark = getCookie("dark")
+    if (dark != "true") {
+        setCookie("dark","true",365)
+    } else {
+        setCookie("dark","false",1)
+    }
+    
+}
